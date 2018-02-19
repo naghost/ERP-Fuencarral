545dea40ed5fab3b025613343aa0a5a3e134f197
Unknown <naghost666@hotmail.com>
2018-02-19 15:56:10 +0100

    Se añade modulo inventario y conexion BBDD

--- ERP-Fuencarral/Inventario/index.php
+++ ERP-Fuencarral/Inventario/index.php
@@ -1,12 +1,12 @@
 <?php
-$titulo="Ejemplo";
+$titulo="Inventario";
 include '../Plantilla/head.php';
 include '../Plantilla/BBDD.php';
 ?>
 <div class="container-fluid">
 	<div class="row">
 	<div class="col-md-2">
-		<h1>hola</h1>
+		
 	</div>
 	<div class="col-md-10">
 		<h1>hola</h1>
